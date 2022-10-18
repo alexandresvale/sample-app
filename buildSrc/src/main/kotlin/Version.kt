f9df1a6a1503b2d2c393b5bd56d4dc995ffca054
Alexandre Vale <alexandre.vale@live.com>
2022-10-17 23:28:44 -0300

    Adicionado dependencia na buildSrc.

---
+++ buildSrc/src/main/kotlin/Version.kt
@@ -0,0 +1,31 @@
+object Version {
+
+    const val androidCoreVersion = "1.9.0"
+    const val androidAppCompat = "1.5.1"
+    const val material = "1.6.1"
+    const val constraintLayout = "2.1.4"
+    const val lifecycle = "2.5.1"
+    const val activityKtx = "1.6.0"
+    //Koin
+    const val koinAndroid = "3.2.2"
+    //Test
+    const val junit = "4.13.2"
+    const val androidTestCore = "1.4.0"
+    const val androidArchTes = "2.1.0"
+    //Android Test
+    const val junitExt = "1.1.3"
+    const val expressoCore = "3.4.0"
+    const val runner = "1.4.0"
+    //Retrofit
+    const val retrofit = "2.4.0"
+    //Gson
+    const val gson = "2.8.9"
+    //Room
+    const val room = "2.4.3"
+    //RxJava
+    const val rxJava = "2.2.7"
+    const val rxKotlin = "2.2.0"
+    const val rxAndroid = "2.1.1"
+    //Mockk
+    const val mockkVersion = "1.13.2"
+}
